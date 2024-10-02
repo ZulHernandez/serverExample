4cc314cae349cedaacd24b851d458411309aab75
Saúl Ulises Hernández Cruz <146755805+SaulHernandezL@users.noreply.github.com>
2024-10-02 10:13:02 -0600

    Refactoriza el código del servidor y corrige la configuración de lanzamiento del navegador

--- server.mjs
+++ server.mjs
@@ -6,7 +6,7 @@ import express, { response } from "express"; //* Importa express
 
 async function mensaje(obj) {
 	// Launch the browser and open a new blank page
-	const browser = await puppeteer.launch({ headless: "true" });
+	const browser = await puppeteer.launch({ headless: "new" });
 	const page = await browser.newPage();
 
 	// Navigate the page to a URL.
